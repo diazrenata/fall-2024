e504e7e34c29f36f92f87d82b34041cd3aca5ead
Renata Diaz <renatadiaz@arizona.edu>
2024-09-26 11:35:56 -0700

    Tuesday's demo

--- data_manipulation_script.R
+++ data_manipulation_script.R
@@ -5,9 +5,7 @@ library(ggplot2)
 
 penguins_data <- penguins
 
-write.csv(penguins_data,
-          "data/penguins.csv",
-          row.names = FALSE)
+write.csv(penguins_data, "data/penguins.csv", row.names = FALSE)
 
 head(penguins)
 str(penguins)
@@ -50,6 +48,48 @@ penguins_rounded <- penguins_data |>
 
 penguin_species_totals <- penguins_data |>
   group_by(species, island) |>
-  summarize(total_penguins = n(),
-            total_penguin_biomass = sum(body_mass_g, na.rm = TRUE)) |>
+  summarize(
+    total_penguins = n(),
+    total_penguin_biomass = sum(body_mass_g, na.rm = TRUE)
+  ) |>
   ungroup()
+
+# Reshaping data
+
+penguins_site_by_species <- penguin_species_totals |>
+  select(species, island, total_penguins)
+
+penguins_site_by_species <- penguin_species_totals |>
+  select(species, island, total_penguins) |>
+  tidyr::pivot_wider(
+    id_cols = island,
+    names_from = species,
+    values_from = total_penguins,
+    values_fill = 0
+  )
+
+penguins_back_to_totals <- penguins_site_by_species |>
+  tidyr::pivot_longer(-island, 
+                      names_to = "Species", 
+                      values_to = "Abundance")
+
+# Joins 
+
+island_coordinates <- data.frame(
+  island = c("Biscoe", "Dream", "Torgersen"),
+  latitude = c(-65.433, -64.733, -64.766),
+  longitude = c(-65.5, -64.344, -64.083)
+)
+
+penguins_coords <- left_join(penguins_data, island_coordinates)
+
+
+
+ggplot(penguins_coords, aes(longitude, latitude, size = body_mass_g, color = species)) +
+  geom_jitter(alpha = .3) +
+  scale_color_viridis_d(option = "mako", begin = .2, end = .8)
+
+
+ggplot(penguins_coords, aes(latitude, body_mass_g, color = species)) +
+  geom_jitter() +
+  scale_color_viridis_d(option = "mako", begin = .2, end = .8)
